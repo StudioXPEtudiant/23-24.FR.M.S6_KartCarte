00a55be8537182576ffae5e5b60581b59ec257bd
Dragoune <paliraesteban@gmail.com>
2025-05-15 19:07:34 +0200

    Choixdescartes

--- kartcart/Bar.gd
+++
@@ -1,7 +0,0 @@
-# Fait de la barre une classe personnalisée reconnue par Godot
-class_name Bar
-extends TextureProgressBar
-
-func add_progress(amount):
-	value += amount
-	value = min(value, max_value)  # Empêche de dépasser 100%
