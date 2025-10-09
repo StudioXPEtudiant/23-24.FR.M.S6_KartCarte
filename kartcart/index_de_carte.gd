93908a22ccce6f68d6c324c5c50f7d8cc7941a47
ThomasDucosXP <t.ducos@mailo.com>
2025-10-09 20:46:42 +0200

    Index de Carte

---
+++ kartcart/index_de_carte.gd
@@ -0,0 +1,24 @@
+extends Node2D
+var ListeCarte:Array
+
+var xOrigine:float = 125
+var yOrigine:float = 10
+var TailCarte:Vector2 = Vector2(576/2,648/2)
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	ListeCarte = $"../ListeCarte".get_children()
+	ListeCarte.size()
+	for i in range(0,ListeCarte.size()):
+		var carte:Node2D = ListeCarte[0].duplicate()
+		carte.visible = true
+		add_child(carte)
+		carte.scale = Vector2(0.5,0.5)
+		carte.position = Vector2(xOrigine+(TailCarte.x+10)*(i%3),yOrigine+(TailCarte.y+10)*(i/3))
+		print(carte)
+		print(carte.position)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
