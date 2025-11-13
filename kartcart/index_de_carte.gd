1820132bf40ce52bcf511332ace2be1759c0bb0a
ThomasDucosXP <t.ducos@mailo.com>
2025-11-13 20:30:54 +0100

    commmmmmit

--- kartcart/index_de_carte.gd
+++ kartcart/index_de_carte.gd
@@ -9,17 +9,33 @@ var TailCarte:Vector2 = Vector2(576/2,648/2)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	print("Début ListeCarte ")
 	ListeCarte = $"../../../../ListeCarte".get_children()
 	ListeCarte.size()
 	for i in range(0,ListeCarte.size()):
 		var carte:Node2D = ListeCarte[i].duplicate()
 		carte.visible = true
-		add_child(carte)
+		var container:BoxContainer = BoxContainer.new()
+		container.add_child(carte)
+		
+		var btn:Button = Button.new()
+		btn.text = "Text buton"
+		#btn.button_down.connect()
+		
+		container.add_child(btn)
+		add_child(container)
+		
 		carte.scale = Vector2(0.5,0.5)
-		carte.position = Vector2(xOrigine+(TailCarte.x+10)*(i%3),yOrigine+(TailCarte.y+10)*(i/3))
+		container.position = Vector2(xOrigine+(TailCarte.x+10)*(i%3),yOrigine+(TailCarte.y+10)*(i/3))
 		print(carte)
-		print(carte.position)
+		print(carte.nom)
+		print(container.position)
+		print("--------------")
+	print("Fin ListeCarte ")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func sourie():
+	print("c'est bon")
