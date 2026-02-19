fee30f9c7254a514f7393580e214f9d8321c9872
Dragoune <paliraesteban@gmail.com>
2026-02-19 19:26:31 +0100

    vvv

---
+++ kartcart/description.gd
@@ -0,0 +1,17 @@
+@tool
+extends Label
+
+
+@export var carte:carte
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	text = carte.Description
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if text != carte.Description:
+		text = carte.Description
+		
---
+++ kartcart/link_button.gd
@@ -0,0 +1,16 @@
+@tool
+extends LinkButton
+
+
+@export var carte:carte
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	text = carte.defEffet
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if text != carte.defEffet:
+		text = carte.defEffet
